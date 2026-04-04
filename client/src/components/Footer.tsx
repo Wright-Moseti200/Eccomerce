288881c595dc8ba47b11fb6b9915afe67c01c6a2
Wright Gichana <wrightgichana@gmail.com>
2026-04-04 13:17:35 +0300

    footer mobile responsive

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,34 +4,40 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <>
-
-
-    <section className='flex flex-col w-full'>
-        <div className='flex justify-around '>
-            <div>
-                <img src={assets.logo} className='h-13'/>
-                <p className='w-120 mt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio alias praesentium quam quis impedit pariatur error minima autem repellat assumenda. Minima illum reprehenderit, nulla corporis laboriosam nesciunt maxime et ad.
-                Harum animi dolor cupiditate aspernatur quidem! Quae officiis facilis voluptates magnam hic. Exercitationem repellendus esse aperiam amet, soluta ipsam dolorem magni ratione, voluptates eum autem fugiat qui a alias voluptatibus?</p>
-            </div>
-            <div>
-                <h1 className='text-2xl font-semibold'>COMPANY</h1>
-                <div className='mt-5 flex flex-col justify-around h-30'>
-                    <p>Home</p>
-                    <p>About us</p>
-                    <p>Delivery</p>
-                    <p>Privacy policy</p>
-                </div>
-            </div>
-            <div>
-                <h1 className='text-2xl font-semibold'>GET IN TOUCH</h1>
-                <div className='mt-5 flex flex-col justify-around h-15'>
-                    <p>+254714471627</p>
-                    <p>wrightgichana@gmail.com</p>
-                </div>
+    <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        
+        <div>
+            <img src={assets.logo} className='h-[35px] sm:h-[45px] mb-5'/>
+            <p className='w-full md:w-2/3 text-gray-600 leading-relaxed text-sm'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio alias praesentium quam quis impedit pariatur error minima autem repellat assumenda. Minima illum reprehenderit, nulla corporis laboriosam nesciunt maxime et ad.
+            Harum animi dolor cupiditate aspernatur quidem! Quae officiis facilis voluptates magnam hic.
+            </p>
+        </div>
+        
+        <div>
+            <h1 className='text-xl font-bold mb-5 text-gray-800'>COMPANY</h1>
+            <div className='flex flex-col gap-2 text-gray-600'>
+                <p className='cursor-pointer hover:text-black'>Home</p>
+                <p className='cursor-pointer hover:text-black'>About us</p>
+                <p className='cursor-pointer hover:text-black'>Delivery</p>
+                <p className='cursor-pointer hover:text-black'>Privacy policy</p>
             </div>
         </div>
-        <p className='text-center mt-6 '>Copyright 2026@Wright.com - All Rights Reserved </p>
-    </section>
+
+        <div>
+            <h1 className='text-xl font-bold mb-5 text-gray-800'>GET IN TOUCH</h1>
+            <div className='flex flex-col gap-2 text-gray-600'>
+                <p>+254714471627</p>
+                <p>wrightgichana@gmail.com</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div>
+        <hr className='border-gray-200' />
+        <p className='text-center py-5 text-sm font-medium text-gray-600'>Copyright 2026@Wright.com - All Rights Reserved</p>
+    </div>
     </>
   )
 }
